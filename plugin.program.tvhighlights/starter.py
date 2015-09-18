769a350cf5dfac361173dd1daeec67c3f8a8c0d9
Tobias D. Oestreicher <tobias.d@oestreicher.com.de>
2015-09-18 09:02:50 +0200

    updated version to 0.0.9

--- plugin.program.tvhighlights/starter.py
+++ plugin.program.tvhighlights/starter.py
@@ -6,7 +6,7 @@
 #        AUTHOR:  Tobias D. Oestreicher
 #
 #       LICENSE:  GPLv3 <http://www.gnu.org/licenses/gpl.txt>
-#       VERSION:  0.0.8
+#       VERSION:  0.0.9
 #       CREATED:  02.09.2015
 #
 ###########################################################################
