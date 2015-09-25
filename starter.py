00eeb05a2efdf7bd946207d9f9015285343671ae
Tobias D. Oestreicher <tobias.d@oestreicher.com.de>
2015-09-25 21:44:39 +0200

    addon.xml fixes

--- starter.py
+++ starter.py
@@ -6,7 +6,7 @@
 #        AUTHOR:  Tobias D. Oestreicher
 #
 #       LICENSE:  GPLv3 <http://www.gnu.org/licenses/gpl.txt>
-#       VERSION:  0.0.9
+#       VERSION:  0.1.0
 #       CREATED:  02.09.2015
 #
 ###########################################################################
